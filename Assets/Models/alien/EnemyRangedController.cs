893d7339086026f4016a2defb49239adb9ae44ec
AchilleasTyrnenopoulos <68752518+AchilleasTyrnenopoulos@users.noreply.github.com>
2020-12-29 22:10:44 +0200

    Navmesh Enemy Ai
    Added navmesh
    Added Graph scripts that use the A* algorithm
    Changed script to use navmesh agent (ai library)

--- Assets/Models/alien/EnemyRangedController.cs
+++ Assets/Models/alien/EnemyRangedController.cs
@@ -1,32 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyRangedController : MonoBehaviour
 {
     #region Variables
     public Transform player;
+
+    
     Animator anim;
+    NavMeshAgent agent;
 
     float rotationSpeed = 2.0f;
-    float speed = 2.0f;
-    float visDist = 20.0f;
-    float visAngle = 30.0f;
-    float shootDist = 15.0f;
+    
+    [Header("LINE OF SIGHT")]
+    public float visDist = 20.0f;
+    public float visAngle = 30.0f;
+    public float shootDist = 15.0f;
 
-     bool isIdle = true;
-     bool isMoving = false;
-     bool isAttacking = false;
+    
+
+    bool isIdle = true;
+    bool isMoving = false;
+    bool isAttacking = false;
 
     //change to enum 
     string state = "IDLE";
+
+    //patrol
+    Transform goal;
+    float patrolAccuracy = 1.0f;
+    public GameObject wpManager;
+    GameObject[] wps;
+
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
+        agent = this.GetComponent<NavMeshAgent>();
+
         anim = GetComponentInChildren<Animator>();
         state = "IDLE";
+
+        wps = wpManager.GetComponent<WPManager>().waypoints;
     }
 
     // Update is called once per frame
@@ -39,7 +57,7 @@ public class EnemyRangedController : MonoBehaviour
         float angle = Vector3.Angle(direction, this.transform.forward);
 
         //test if npc sees player
-        if(direction.magnitude < visDist && angle < visAngle)
+        if (direction.magnitude < visDist && angle < visAngle)
         {
             //set direction y to zero (so npc wont tilt)
             direction.y = 0;
@@ -50,13 +68,13 @@ public class EnemyRangedController : MonoBehaviour
             //test if in range to shoot
             if (direction.magnitude > shootDist)
             {
-                if (state != "RUNNING") 
+                if (state != "RUNNING")
                 {
                     state = "RUNNING";
                     isIdle = false;
                     isAttacking = false;
                     isMoving = true;
-                    
+
                 }
             }
             else
@@ -79,18 +97,38 @@ public class EnemyRangedController : MonoBehaviour
                 isAttacking = false;
                 isIdle = true;
             }
-            
+
         }
 
         //running state
         if (state == "RUNNING")
-        {            
-            this.transform.Translate(0, 0, Time.deltaTime * speed);
+        {
+            //this.transform.Translate(0, 0, Time.deltaTime * speed);
+            agent.SetDestination(player.transform.position);
+        }
+        else 
+        { 
+            agent.SetDestination(this.transform.position); 
         }
 
         //handle animations
         anim.SetBool("isIdle", isIdle);
-        anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isMoving", isMoving);        
         anim.SetBool("isAttacking", isAttacking);
+    }
+
+    //FUNCTIONS
+    void TriggerAnimParameter(bool isDoing)
+    {
+        bool[] anims = {isIdle, isMoving, isAttacking};
+        string isDoingStr = isDoing.ToString();
+        for (int i = 0; i < anims.Length; i++)
+        {
+            string tempBool = anims[i].ToString();
+            if (tempBool != isDoingStr)
+                anims[i] = false;
+        }
+
+        isDoing = true;
     }
 }
